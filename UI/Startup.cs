5316cd7fa7a5b656e51523d03d5ba408b3ef8cdc
TufanOzdemir <amonisal6@gmail.com>
2018-09-25 22:26:46 +0300

    IdentityDBContext Data projesine taşındı.
    Migration silindi.

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Data.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using UI.Data;
-using UI.Models;
 using UI.Services;
 
 namespace UI
