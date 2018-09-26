ce28d369f582dd48fd57fe95f295340472aaa89d
TufanOzdemir <amonisal6@gmail.com>
2018-09-26 17:56:37 +0300

    Controllerlar eklendi.

---
+++ UI/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Models;
+using Interface.Result;
+using Interface.ServiceInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service.IdentityServices;
+using UI.ViewModels;
+
+namespace UI.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            Result<List<CategoryDO>> categoryList = _categoryService.GetAll();
+            return View(categoryList.Data);
+        }
+
+        public IActionResult Create()
+        {
+            CategoryCreateViewModel categoryCreateViewModel = new CategoryCreateViewModel();
+            Result<List<CategoryDO>> categoryList = _categoryService.GetAll();
+            categoryCreateViewModel.CategoryList = categoryList.Data ?? new List<CategoryDO>();
+            categoryCreateViewModel.CategoryDO = new CategoryDO();
+            return View(categoryCreateViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryCreateViewModel model)
+        {
+            _categoryService.Create(model.CategoryDO);
+            return Redirect("Index");
+        }
+    }
+}
---
+++ UI/ViewModels/CategoryCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.ViewModels
+{
+    public class CategoryCreateViewModel
+    {
+        public CategoryCreateViewModel()
+        {
+            CategoryList = new List<CategoryDO>();
+        }
+
+        public CategoryDO CategoryDO { get; set; }
+        public List<CategoryDO> CategoryList { get; set; }
+    }
+}
